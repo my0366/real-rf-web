0329a7f5a6cec9f5efcdcc32dfb2212958f5b523
my0366 <rlacnrrnrhd@naver.com>
2025-09-05 11:20:55 +0900

    Add Favicon

--- src/routes/menu.ts
+++ src/routes/menu.ts
@@ -1,38 +1,37 @@
 export interface MenuProps {
-    label: string;
-    icon: string;
-    path: string;
-    description: string;
+  label: string;
+  icon: string;
+  path: string;
+  description: string;
 }
 
 export const menuItems: MenuProps[] = [
-    {
-        label: '테스트',
-        icon: '🎯',
-        path: '/',
-        description: 'RF 테스트'
-    },
-    {
-        label: '주제 관리',
-        icon: '📚',
-        path: '/topics',
-        description: '주제 추가 / 수정 / 삭제'
-    },
-    {
-        label: '질문 관리',
-        icon: '📝',
-        path: '/questions',
-        description: '질문 등록 / 수정/ 삭제'
-    },
+  {
+    label: '테스트',
+    icon: '🎯',
+    path: '/',
+    description: 'RF Check',
+  },
+  {
+    label: '주제 관리',
+    icon: '📚',
+    path: '/topics',
+    description: '주제 추가 / 수정 / 삭제',
+  },
+  {
+    label: '질문 관리',
+    icon: '📝',
+    path: '/questions',
+    description: '질문 등록 / 수정/ 삭제',
+  },
 ];
 
 // 관리자 전용 메뉴
 export const adminMenuItems: MenuProps[] = [
-    {
-        description: '회원가입 승인 및 관리',
-        path: '/admin/users',
-        label: '회원가입 승인',
-        icon: '👥'
-    }
+  {
+    description: '회원가입 승인 및 관리',
+    path: '/admin/users',
+    label: '회원가입 승인',
+    icon: '👥',
+  },
 ];
-
